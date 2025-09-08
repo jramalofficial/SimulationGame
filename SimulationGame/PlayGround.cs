92bcf88722615cbfb702e688f86dd67b8b12814d
amaljanardhanan <amal.janardhanan@oneteamus.com>
2025-09-08 11:06:54 +0530

    new commit

--- SimulationGame/PlayGround.cs
+++ SimulationGame/PlayGround.cs
@@ -21,7 +21,7 @@ namespace SimulationGame
         public void StartGame()
         {
             var player = new Player();
-            Console.WriteLine("\nChoose your vehicle: c = Car, b = Bus, o= get out of the vehicle");
+            Console.WriteLine("\nChoose your vehicle: c = Car, b = Bus, o= get out of vehicle");
 
             while (true)
             {
